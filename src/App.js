4ebbc29c86198c16af2461310a20af415ede2533
Andre Furquim <=>
2020-12-03 02:07:31 -0300

    Render articles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import AppMenu from './Components/AppMenu';
+import ArticleList from './Components/ArticleList';
+import ArticleService from './Services/ArticleService';
+import { Divider } from '@material-ui/core';
 
 const App = () => {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    ArticleService.getMostViewedArticles()
+      .then(res => {
+        setArticles(res.data.results);
+      })
+
+  }, [])
   return (
     <>
       <Grid container spacing={2}>
         <AppMenu brand='McFadyen News' />
+        <Grid item xs={12}>
+          <ArticleList title='Featured News' articles={articles} />
+          <Divider />
+          <ArticleList showImages={false} title='Latest News' articles={articles} />
+          </Grid>
       </Grid>
     </>
   )
